46686a30b34807a8c85ec63dbda7997f64d5a2cd
Akshit Mangotra <akshitmangotra@gmail.com>
2021-11-26 18:07:56 +0000

    Added Efficient Approach for DivisorOfNum

--- Mathematics/8_DivisorsofNum.cpp
+++ Mathematics/8_DivisorsofNum.cpp
@@ -9,9 +9,27 @@ void BruteForce(int n){
 					cout<<i<<" ";
 				}
 		}
+		cout<<endl;
 }
 
-void Optmi
+void OptimizeOne(int n){
+	// It is based on 2 facts:
+	// 1) Divisors of the number are always in pair.
+	// 2) One of the divisor in every pair is <= Root(n)
+	vector<int> ans;
+	for(int i =1;i*i<=n;i++){
+		if(n%i ==0){
+			ans.push_back(i);
+
+		if((n/i)!= i)
+			ans.push_back(n/i);
+		}
+	}
+	sort(ans.begin(), ans.end());
+	for(int i:ans)
+			cout<<i<<" ";
+	cout<<endl;
+}
 
 int main(){
 int t;
@@ -20,5 +38,6 @@ cin>>t;
 		int n;
 		cin>>n;
 		BruteForce(n);
+		OptimizeOne(n);
 	}
 }
