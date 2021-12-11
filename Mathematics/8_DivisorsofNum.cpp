58c5bc68f42b577987b4576c51d3dae0aea1623e
Akshit Mangotra <akshitmangotra@gmail.com>
2021-12-12 02:36:33 +0530

    Added optimal approch for DivisorsOfNum

--- Mathematics/8_DivisorsofNum.cpp
+++ Mathematics/8_DivisorsofNum.cpp
@@ -6,6 +6,9 @@ using namespace std;
 // 1) If a number n is a perfect square.
 // 2) If root(n) is a factor of N. Make sure not to print it 2 times.
 
+// Note: For finding nearby root of any number N. Find the lowest nearby perfect square value.
+// Eg: Root 50 approx =  Root(49) = 7.
+
 void BruteForce(int n){
 
 		for(int i = 1;i<=n;i++){
@@ -15,8 +18,8 @@ void BruteForce(int n){
 		}
 		cout<<endl;
 }
-
-void OptimizeOne(int n){// TC:O(Root(n)) SC: O(N)
+// 50: 1 2 5 10 25 50: 
+void OptimizeOne(int n){// TC:O(Root(n)  + NlogN) [if in sorted form] else TC: O(sqrt(n)),  SC: O(N)
 	// It is based on 2 facts:
 	// 1) Divisors of the number are always in pair.
 	// 2) One of the divisor in every pair is <= Root(n)
@@ -24,18 +27,18 @@ void OptimizeOne(int n){// TC:O(Root(n)) SC: O(N)
 	for(int i =1;i*i<=n;i++){
 		if(n%i ==0){
 			ans.push_back(i);
-
+			
 		if((n/i)!= i)
 			ans.push_back(n/i);
 		}
 	}
-	sort(ans.begin(), ans.end());
+	sort(ans.begin(), ans.end()); // O(nLogn) extra as we need to output the answer in increasing sequence.
 	for(int i:ans)
 			cout<<i<<" ";
 	cout<<endl;
 }
 
-void OptimizeFurther(int n){
+void OptimizeFurther(int n){ // TC: O(n) SC: O(1)
 
 		// Edge case: Square Numbers. Eg: 25
 		// If the root(n) is a factor of N. Eg: 30.
@@ -45,10 +48,10 @@ void OptimizeFurther(int n){
 						cout<<i<<" ";
 		}
 
-		i--;
+		i--; // For bringing back the incremented i back to = sqrt(i)
 
 		for( ; i>=1 ; i--){ // No initialization.
-				if(n%i==0 && i!=(n/i))
+				if(n%i==0 && i!=(n/i)) // use full for case where i = n/i = sqrt(n). Eg: 5 = 25/5 = root(5)
 					cout<<(n/i)<<" ";
 		}
 }
