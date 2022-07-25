46f8c61b944e88474af1cad83097649e83ea8bbf
Akshit Mangotra <akshitmangotra@gmail.com>
2022-07-26 05:06:39 +0530

    Renamed Recursion Files to Format Directory Structure

---
+++ 3_Recursion/01_Print1toN.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+void fun1toN(int i, int n){ // TC: O(n) as we calling n functions. SC: O(n) as we put at max N fun calls() into stack.
+    if(i > n)
+        return;
+    cout<<"printing "<<i<<endl;
+    fun1toN(i+1,n);
+}
+
+// Try running the program and then see the output to see the backtracking working
+void Backtrack_1_to_N(int n){ // TC: O(n) Sc: O(n)
+    cout<<"f("<<n<<") is called\n";
+    if(n < 1)
+        return;
+    cout<<"calling f("<<n-1<<")\n";
+    Backtrack_1_to_N(n-1);
+    cout<<n<<endl;
+}
+int main(){
+    int n;
+    cin>>n;
+    cout<<"printing 1 to N without backtracking"<<endl;
+    fun1toN(1,n);
+    cout<<"printing 1 to N with backtracking"<<endl;
+    Backtrack_1_to_N(n);
+    return 0;
+}
